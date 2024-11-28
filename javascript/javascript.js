9729073c8b0227eb2ccc47d4f34fbc6f8dbbf132
bimela tony <tonybimela@gmail.com>
2024-11-28 11:48:13 +0100

    adding changes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,33 +1,27 @@
-
-let input = document.getElementById('button-number');
-let btn = document.querySelectorAll('button');
+let input = document.getElementById("button-number");
+let btn = document.querySelectorAll("button");
 
 let string = "";
 let arr = Array.from(btn);
-console.log(arr)
-arr.forEach(button => {
-console.log(button)
-  
-    button.addEventListener('click', (e) =>{
-console.log(e)
+console.log(arr);
+arr.forEach((button) => {
+  console.log(button);
 
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
+  button.addEventListener("click", (e) => {
+    console.log(e);
 
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
-        }
-        
-    })
-})
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
