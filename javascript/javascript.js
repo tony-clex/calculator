6df8e659b48b752fb9170658eb6897fbe645fa4e
bimela tony <tonybimela@gmail.com>
2025-03-26 13:11:11 +0100

    addingchanges

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,27 +1,27 @@
-let input = document.getElementById("button-number");
-let btn = document.querySelectorAll("button");
+const input = document.getElementById('button-number');
+const btn = document.querySelectorAll("button");;
 
-let string = "";
-let arr = Array.from(btn);
-console.log(arr);
+let string = "";;
+let arr = Array.from(btn);;
+console.log(arr);;
 arr.forEach((button) => {
   console.log(button);
 
-  button.addEventListener("click", (e) => {
-    console.log(e);
+  button.addEventListener('click', (e) => {
+    console.log(e);;
 
-    if (e.target.innerHTML == "=") {
-      string = eval(string);
-      input.value = string;
-    } else if (e.target.innerHTML == "AC") {
-      string = "";
-      input.value = string;
-    } else if (e.target.innerHTML == "DEL") {
-      string = string.substring(0, string.length - 1);
-      input.value = string;
+    if (e.target.innerHTML === "=") {
+      string = eval(string);;
+      input.value = string;;
+    } else if (e.target.innerHTML === 'AC') {
+      string = '';;
+      input.value = string;;
+    } else if (e.target.innerHTML === 'DEL') {
+      string = string.substring(0, string.length - 1);;
+      input.value = string;;
     } else {
-      string += e.target.innerHTML;
-      input.value = string;
+      string += e.target.innerHTML;;
+      input.value = string;;
     }
-  });
-});
+  });;
+});;
